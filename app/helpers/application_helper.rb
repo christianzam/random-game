2ebcfa7b41faa43a142a555695c79857ffc4e129
Christian Zamora <christian@diagnostikare.com>
2023-08-10 14:58:43 -0500

    updated project name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
     if user.profile_pic.attached?
       image_tag user.profile_pic, class: "placeholder-image #{class_name}", alt: 'user_pic'
     else
-      image_tag 'fa-solid fa-user', class: "placeholder-image #{class_name}", alt: 'user_pic'
+      image_tag 'user_logo', class: "placeholder-image #{class_name}", alt: 'user_pic'
     end
   rescue StandardError
-    image_tag 'gifts.png', class: 'placeholder-image', alt: 'user_pic'
+    image_tag 'user_logo.png', class: 'placeholder-image', alt: 'user_pic'
   end
 end
